c2f994cfe4bb80f791a9220614b757af880f7543
Saul Ceja <saulceja08@gmail.com>
2023-04-26 15:21:36 -0700

    Complete Lab

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Your code goes here
+document.addEventListener('DOMContentLoaded', function() {
+    let currentIDText = document.getElementById('text')
+    currentIDText.textContent = 'This is really cool!'
+})
